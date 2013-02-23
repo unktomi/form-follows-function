d147ad33990b8377332e92f39396ba7c18fec7c6
christopher.d.oliver@gmail.com <christopher.d.oliver@gmail.com@9cb737ae-64f5-ec86-17c0-98e80c34e615>
2013-02-23 22:16:42 +0000

    Inching

--- f3_cg_jogl_awt/demo.sh
+++ f3_cg_jogl_awt/demo.sh
@@ -2,4 +2,4 @@
 ln -s -f /Library/Frameworks/Awesomium.framework .
 export LIB=./lib
 export DYLD_LIBRARY_PATH=$LIB/macosxuniversal
-java  -d32 -Xmx512M -classpath ${LIB}/f3rt.jar:${LIB}/jogl.all.jar:${LIB}/gluegen-rt.jar:${LIB}/jogl.cg.jar:${LIB}/f3.svg.awt.jar:${LIB}/f3.awesomium.jogl.jar:build/f3.cg.jogl.awt.jar org.f3.runtime.Main f3.jogl.awt.JoglStage $*
+java  -Df3.debug.anim=false -d32 -Xmx512M -classpath ${LIB}/f3rt.jar:${LIB}/jogl.all.jar:${LIB}/gluegen-rt.jar:${LIB}/jogl.cg.jar:${LIB}/f3.svg.awt.jar:${LIB}/f3.awesomium.jogl.jar:build/f3.cg.jogl.awt.jar org.f3.runtime.Main f3.jogl.awt.JoglStage $*
