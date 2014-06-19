d7e7bf5d3d4ae5cb81bf44c8c8fdb3d774e747a4
christopher.d.oliver@gmail.com <christopher.d.oliver@gmail.com@9cb737ae-64f5-ec86-17c0-98e80c34e615>
2014-06-19 00:16:01 +0000

    inching

--- src/share/classes/f3/lang/ObservableRef.java
+++ src/share/classes/f3/lang/ObservableRef.java
@@ -2,5 +2,5 @@ package f3.lang;
 import org.f3.functions.*;
 
 public interface ObservableRef<a> extends Ref<a> {
-    public <b> Function0<Void> onReplace(final Function1<? extends b, ? super a> f);
+    public <b> Function0<? extends Void> onReplace(final Function1<? extends b, ? super a> f);
 }
